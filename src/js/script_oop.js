a26bb7dfb1a06b8caf04da6e2a041a3ee0da4390
Michal Gebski <gebski.michal@gmail.com>
2022-06-26 16:24:51 +0200

    Change to OOP stule

--- src/js/script_oop.js
+++ src/js/script_oop.js
@@ -36,85 +36,84 @@ const templates = {
   book: Handlebars.compile(document.querySelector(select.templates.book).innerHTML),
 };
 
-const globals = {
-  favouriteBooks: [],
-  filters: [],
-};
+class BooksList {
+  constructor() {
+    const bookApp = this;
 
+    bookApp.favourites = [];
+    bookApp.filters = [];
 
-class Book{
-  constructor(id, data) {
-    const thisBook = this;
-
-    thisBook.id = id;
-    thisBook.data = data;
-
-    thisBook.render();
+    bookApp.getElements();
+    bookApp.initData();
+    bookApp.initBooks();
+    bookApp.filterBooks();
+    bookApp.initActions();
+    bookApp.initFiltering();
+  }
+    
+  initData() {
+    this.data = dataSource;
+    // console.log(this.data);
   }
 
-  render() {
+  getElements() {
+    const bookApp = this;
 
-    const thisBook = this;
-
-    thisBook.data.ratingBgc = app.determineRatingBgc(thisBook.data.rating);
-    thisBook.data.ratingWidth = thisBook.data.rating * 10;
-
-    // generatedHTML based on template
-    const generatedHTML = templates.book(thisBook.data);
-    
-    // create a DOMelement with utils.createElementFromHTML
-    thisBook.element = utils.createDOMFromHTML(generatedHTML);
-    
-    // find the menu container and insert in DOM element in
-    const bookList = document.querySelector(select.container.booksList);
-    
-    // insert the created DOMelement into menu container
-    bookList.appendChild(thisBook.element);
+    bookApp.dom = {};
+    bookApp.dom.form = document.querySelector(select.container.form);
+    bookApp.dom.bookList = document.querySelector(select.container.booksList);
   }
-}
 
-const app = {
+  initBooks() {
+    const bookApp = this;
+    console.log(bookApp);
 
-  initData: function () {
+    for (const book of bookApp.data.books) {
+
+      book.ratingBgc = bookApp.determineRatingBgc(book.rating);
+      book.ratingWidth = bookApp.data.rating * 10;
+
+      // generatedHTML based on template
+      const generatedHTML = templates.book(book);
+    
+      // create a DOMelement with utils.createElementFromHTML
+      book.element = utils.createDOMFromHTML(generatedHTML);
+    
+      // find the menu container and insert in DOM element in container
+      bookApp.dom.bookList.appendChild(book.element);
+    }
+  }  
+
+    
+  initActions() {
     const thisApp = this;
-    thisApp.data = dataSource;
-  },
-  
-  initBooks: function () {
-    const thisApp = this;
-
-    (thisApp.data.books).forEach(book => new Book(book.id, book));
-  },
-  
-  initActions: function () {
     const bookList = document.querySelector(select.container.booksList);
-
+    
     bookList.addEventListener('dblclick', function (event) {
       event.preventDefault();
-
+    
       if (event.target && event.target.offsetParent.matches(select.book.bookImageLink)) {
         const targetElement = event.target.offsetParent;
         const dataId = parseInt(targetElement.getAttribute('data-id'));
-        
+            
         //* clicked book to favourite
         if (!targetElement.classList.contains(classNames.book.favorite)) {
           targetElement.classList.add(classNames.book.favorite);
-          
+              
           //* data-id add to favBooks array
-          globals.favouriteBooks.push(dataId);
+          thisApp.favouriteBooks.push(dataId);
         } else {
           targetElement.classList.remove(classNames.book.favorite);
-          const index = globals.favouriteBooks.indexOf(dataId);
-          globals.favouriteBooks.splice(index, 1);
+          const index = thisApp.favouriteBooks.indexOf(dataId);
+          thisApp.favouriteBooks.splice(index, 1);
         }
       }
     });
-  },
+  }
 
-  initFiltering: function () {
+  initFiltering() {
     const thisApp = this;
 
-    // const form = document.querySelector(select.container.form);
     const form = document.querySelector('.filters');
 
     form.addEventListener('click', function (event) {
@@ -122,27 +121,27 @@ const app = {
       if (event.target.tagName === 'INPUT' && event.target.type === 'checkbox' && event.target.name === 'filter') {
         switch (event.target.checked) {
         case true:
-          globals.filters.push(event.target.value);
+          thisApp.filters.push(event.target.value);
           break;
         case false:
           // eslint-disable-next-line no-case-declarations
-          const index = globals.filters.indexOf(event.target.value);
-          globals.filters.splice(index, 1);
+          const index = thisApp.filters.indexOf(event.target.value);
+          thisApp.filters.splice(index, 1);
           break;
         }
       }
       thisApp.filterBooks();
-      console.log(globals.filters);
+      console.log(thisApp.filters);
     });
-  },
-
-  filterBooks: function () {
+  }
+    
+  filterBooks() {
     const thisApp = this;
 
     for (let book of thisApp.data.books) {
       let shouldBeHidden = false;
 
-      for (let filter of globals.filters) {
+      for (let filter of thisApp.filters) {
         if (book.details[filter]){
           shouldBeHidden = true;
           break;
@@ -154,12 +153,12 @@ const app = {
         document.querySelector('.book__image[data-id="' + book.id + '"]').classList.remove(classNames.book.hidden);
       }
     }
-  },
-
-  determineRatingBgc: function (rating) {
+  }
+    
+  determineRatingBgc(rating) {
     let background = '';
     if (rating < 6) {
-      background = 'linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%);';
+      background = 'linear-gradient(to bottom, #fefcea 0%, #f1da36 100%);';
     } else if (rating > 6 && rating <= 8) {
       background = 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%);';
     } else if (rating > 8 && rating <= 9) {
@@ -168,19 +167,7 @@ const app = {
       background = 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%);';
     }
     return background;
-  },
-
-  init: function () {
-    const thisApp = this;
-    console.log('*** App starting ***');
-    
-    
-    thisApp.initData();
-    thisApp.initBooks();
-    thisApp.filterBooks();
-    thisApp.initActions();
-    thisApp.initFiltering();
-  },
-};
-
-app.init();
+  }
+}
+  
+new BooksList();
